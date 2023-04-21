4b5485fb5e73334acbe63996a744adf112f726c5
Cesario87 <ces.albred@gmail.com>
2023-04-21 19:29:11 +0200

    fixed/get&post

--- src/Controller/Api/UsersController.php
+++ src/Controller/Api/UsersController.php
@@ -2,22 +2,42 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\Request;
-
 
 class UsersController extends AbstractFOSRestController
 {
     /**
-     * @Rest\Get(path="/books")
+     * @Rest\Get(path="/users")
      * @Rest\View(serializerGroups={"user"}, serializerEnableMaxDepthChecks=true)
      */
-    public function getActions(
+    public function getAction(
         UserRepository $userRepository
     ){
         return $userRepository->findAll();
     }
 
+        /**
+     * @Rest\Post(path="/users")
+     * @Rest\View(serializerGroups={"user"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function postAction(
+        EntityManagerInterface $em
+    ){
+        $user = new User();
+        $user->setNombre('Amador');
+        $user->setApellidos('Rivas');
+        // $user->setPoblación('Madrid');
+        // $user->setCategoría('User');
+        // $user->setEdad('55');
+        // $user->setActivo('No');
+        // $createdAt = new \DateTimeImmutable();
+        // $user->setCreatedAt($createdAt);
+        $em->persist($user);
+        $em->flush();
+        return $user;
+    }
 }
